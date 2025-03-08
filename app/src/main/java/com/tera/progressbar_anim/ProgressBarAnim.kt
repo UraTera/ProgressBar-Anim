0b59dde0ddc4311b943c82d1dbd782ae1643c7c7
UraTera <86476079+UraTera@users.noreply.github.com>
2025-03-08 07:04:02 +0300

    Editing

--- app/src/main/java/com/tera/progressbar_anim/ProgressBarAnim.kt
+++ app/src/main/java/com/tera/progressbar_anim/ProgressBarAnim.kt
@@ -18,8 +18,7 @@ import android.view.View
 import android.view.animation.LinearInterpolator
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
-import kotlin.math.asin
-import kotlin.math.min
+import kotlin.math.*
 
 class ProgressBarAnim(
     context: Context,
